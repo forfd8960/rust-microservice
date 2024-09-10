19a83beee48a0ddd7a6284666bcb7fdbac7807db
forfd8960 <forfd8960@gmail.com>
2024-09-10 21:47:35 +0800

    feat: sqlx bulk insert

--- user-stat/examples/gen.rs
+++ user-stat/examples/gen.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
+use anyhow::Result;
 use chrono::{DateTime, Days, Utc};
 use fake::faker::chrono::en::DateTimeBetween;
 use fake::faker::internet::en::SafeEmail;
@@ -6,15 +10,16 @@ use fake::{Dummy, Fake, Faker};
 use nanoid::nanoid;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
+use sqlx::{Executor, PgPool};
 
-#[derive(Debug, Clone, Dummy, Deserialize, Serialize)]
+#[derive(Debug, Clone, Dummy, Deserialize, Serialize, PartialEq, Eq)]
 enum Gender {
     Male,
     Female,
     Unknown,
 }
 
-#[derive(Debug, Clone, Dummy, Deserialize, Serialize)]
+#[derive(Debug, Clone, Dummy, Deserialize, Serialize, PartialEq, Eq)]
 struct UserStats {
     #[dummy(faker = "UniqueEmail")]
     email: String,
@@ -61,9 +66,51 @@ struct UserStats {
     last_sms_notification: DateTime<Utc>,
 }
 
-fn main() {
+impl Hash for UserStats {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.email.hash(state);
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let user_stats = Faker.fake::<UserStats>();
     println!("{:?}", user_stats);
+
+    let pool = PgPool::connect("postgres://postgres:postgres@localhost:5432/user_stats").await?;
+
+    for i in 1..=50 {
+        let users: HashSet<_> = (0..1000).map(|_| Faker.fake::<UserStats>()).collect();
+        bulk_insert(users, &pool).await?;
+        println!("Batch: {}, Inserted {} users", i, i * 1000);
+    }
+
+    Ok(())
+}
+
+async fn bulk_insert(users: HashSet<UserStats>, pool: &sqlx::PgPool) -> Result<()> {
+    let mut tx = pool.begin().await?;
+    for user in users {
+        let query = sqlx::query(
+            r#"INSERT INTO user_stats (email, name, created_at, last_visited_at, last_watched_at, recent_watched, viewed_but_not_started, started_but_not_finished, finished, last_email_notification, last_in_app_notification, last_sms_notification) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)"#
+        )
+        .bind(&user.email)
+        .bind(&user.name)
+        .bind(&user.created_at)
+        .bind(&user.last_visited_at)
+        .bind(&user.last_watched_at)
+        .bind(&user.recent_watched)
+        .bind(&user.viewed_but_not_started)
+        .bind(&user.started_but_not_finished)
+        .bind(&user.finished)
+        .bind(&user.last_email_notification)
+        .bind(&user.last_in_app_notification)
+        .bind(&user.last_sms_notification);
+
+        tx.execute(query).await?;
+    }
+
+    Ok(())
 }
 
 fn start(days: u64) -> DateTime<Utc> {
