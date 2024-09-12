dea7aa7da092c57c683164dcfad2f4fe58c8b5bd
forfd8960 <forfd8960@gmail.com>
2024-09-12 08:40:14 +0800

    feat: user-stat - lib - impl UserStatsService

--- user-stat/src/lib.rs
+++ user-stat/src/lib.rs
@@ -1,1 +1,44 @@
+pub mod abi;
 pub mod pb;
+
+use futures::Stream;
+use pb::{user_stats_server::UserStats, QueryRequest, RawQueryRequest, User};
+use sqlx::PgPool;
+use std::{ops::Deref, pin::Pin, sync::Arc};
+use tonic::{async_trait, Request, Response, Status};
+
+type ServiceResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<User, Status>> + Send>>;
+
+#[derive(Clone)]
+pub struct UserStatsService {
+    inner: Arc<UserStatsServiceInner>,
+}
+
+pub struct UserStatsServiceInner {
+    pool: PgPool,
+}
+
+#[async_trait]
+impl UserStats for UserStatsService {
+    type QueryStream = ResponseStream;
+    type RawQueryStream = ResponseStream;
+
+    async fn query(&self, request: Request<QueryRequest>) -> ServiceResult<Self::QueryStream> {
+        todo!()
+    }
+
+    async fn raw_query(
+        &self,
+        request: Request<RawQueryRequest>,
+    ) -> ServiceResult<Self::RawQueryStream> {
+        todo!()
+    }
+}
+
+impl Deref for UserStatsService {
+    type Target = UserStatsServiceInner;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
