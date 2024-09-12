b85c8266ce18e3467296dc0fe1392894bc9c3996
forfd8960 <forfd8960@gmail.com>
2024-09-12 09:38:36 +0800

    feat: user-stat - abi - impl UserStatsService

--- user-stat/src/abi/mod.rs
+++ user-stat/src/abi/mod.rs
@@ -1,1 +1,34 @@
-// impl UserStatsService {}
+use std::fmt;
+
+// use itertools::Itertools;
+use tonic::{Response, Status};
+
+use crate::{
+    pb::{QueryRequest, RawQueryRequest, User},
+    ResponseStream, ServiceResult, UserStatsService,
+};
+
+impl UserStatsService {
+    pub async fn query(&self, query: QueryRequest) -> ServiceResult<ResponseStream> {
+        let sql = query.to_string();
+        self.raw_query(RawQueryRequest { query: sql }).await
+    }
+
+    pub async fn raw_query(&self, req: RawQueryRequest) -> ServiceResult<ResponseStream> {
+        let Ok(ret) = sqlx::query_as::<_, User>(&req.query)
+            .fetch_all(&self.inner.pool)
+            .await
+        else {
+            return Err(Status::internal(format!("{}", req.query)));
+        };
+
+        let stream = futures::stream::iter(ret.into_iter().map(Ok));
+        Ok(Response::new(Box::pin(stream)))
+    }
+}
+
+impl fmt::Display for QueryRequest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SELECT email, name FROM user_stats WHERE ")
+    }
+}
